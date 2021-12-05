a158368fa1e81fa97832146503e4616b55d6668b
noamroy <80029502+noamroy@users.noreply.github.com>
2021-12-05 09:25:30 +0200

    define lights, traffic, users data
    define light, traffic, users jsons and models

---
+++ data/data.js
@@ -0,0 +1,8 @@
+const trafficSystems = require('./trafficSystems');
+const users = require('./users');
+const lightSystems = require('./lightSystems');
+module.exports = {
+    trafficSystems ,
+    lightSystems,
+    users
+};
