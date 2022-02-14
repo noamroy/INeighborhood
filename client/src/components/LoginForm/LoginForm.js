0dad9e04becdc3c88db0bd482d942b7f5ba2e39b
misha <misha@DESKTOP-4F9V7UT>
2022-02-14 23:21:44 +0200

    Login redirect

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,5 +1,5 @@
 //IMPORTS
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 //CSS
 import Form from "react-bootstrap/Form";
@@ -11,60 +11,67 @@ import constants from "../../static/constants";
 export default function Login() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  const [redirect,setRedirect] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (redirect) {
+      navigate(`/dashboard`);
+    }
+  }, [redirect]);
 
   function validateForm() {
     return name.length > 2 && password.length > 2;
   }
-  let navigate = useNavigate();
-  navigate('/dashboard');
+
+
   async function handleSubmit(event) {
     event.preventDefault();
-    const info= {"name": name, "password": password};
+    const info = { "name": name, "password": password };
     const loginResponse = await fetch(`${constants.hostNoam}user/login`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(info)
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(info)
     });
     const loginResponseJson = await loginResponse.json();
     console.log(JSON.stringify(loginResponseJson));
-    if (loginResponseJson.status!=200){
-        alert("Wrong name or password please try again");
+    if (loginResponseJson.status != 200) {
+      alert("Wrong name or password please try again");
     } else {
-        localStorage.setItem('name', loginResponseJson.name);
-        localStorage.setItem('token', loginResponseJson.token);
-        localStorage.setItem('group', loginResponseJson.group);
-        alert("SUCCESS LOGIN");
-        setRedirect(true);
+      localStorage.setItem('name', loginResponseJson.name);
+      localStorage.setItem('token', loginResponseJson.token);
+      localStorage.setItem('group', loginResponseJson.group);
+      alert("Login succesful");
+      setRedirect(true);
     }
   }
+    return (
+      <div className="Login">
+        <Form>
+          <Form.Group size="lg" controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              autoFocus
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size="lg" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button block size="lg" type="submit" disabled={!validateForm()} onClick={handleSubmit}>
+            Login
+          </Button>
+        </Form>
+      </div>
+    );
 
-  return (
-    <div className="Login">
-      <Form>
-        <Form.Group size="lg" controlId="name">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            autoFocus
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()} onClick={handleSubmit}>
-          Login
-        </Button>
-      </Form>
-    </div>
-  );
 }
