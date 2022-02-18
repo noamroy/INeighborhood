ab6fd913da47015ddcced36da6b39ac1122c4164
misha <misha@DESKTOP-4F9V7UT>
2022-02-18 18:29:27 +0200

    Working map with location press

--- client/src/components/GoogleMap/GoogleMap.js
+++ client/src/components/GoogleMap/GoogleMap.js
@@ -2,19 +2,9 @@ import React, { useState } from "react";
 import './GoogleMap.scss';
 
 function GoogleMap(props) {
-    const [location, setLocation] = useState("shenkar%20college");
-    const [url, setUrl] = useState(`https://maps.google.com/maps?q=shenkar%20college&t=&z=17&ie=UTF8&iwloc=&output=embed`);
-
-    React.useEffect(() => {
-        setUrl(`https://maps.google.com/maps?q=${location}&t=&z=17&ie=UTF8&iwloc=&output=embed`)
-    }, []);
 
     return (
-        <section className="map">
-            <div className="responsive-map mapdiv">
-                <iframe id="map" classnName="iframe-map" src={url} frameBorder="0" style={{border:0}} allowFullScreen></iframe>
-            </div>
-        </section>
+        <iframe id="map" classnName="iframe-map" src={`https://maps.google.com/maps?q=${props.location}&t=&z=17&ie=UTF8&iwloc=&output=embed`} frameBorder="0" allowFullScreen></iframe>
     );
 }
 
