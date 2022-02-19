aede90f5451413a1f0fcd929ea2a1b24f69df366
misha <misha@DESKTOP-4F9V7UT>
2022-02-19 09:57:20 +0200

    Server delte and postman addd

--- server/data/data.js
+++
@@ -1,8 +0,0 @@
-const neighborhoodSystems = require('./neighborhoodSystems');
-const programs = require('./programs');
-const users = require('./users');
-module.exports = {
-    neighborhoodSystems ,
-    programs,
-    users
-};
